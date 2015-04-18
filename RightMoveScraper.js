6079810d07c80039fab19f4c4030e8536fd485ff
Joe <joseph.reeve@googlemail.com>
2015-04-18 18:31:06 +0100

    Added closes to modals

--- RightMoveScraper.js
+++ RightMoveScraper.js
@@ -9,13 +9,13 @@ db.rightmoveCodes.find({}, function(err, docs){
 
 module.exports = {
   byOutcode:function(outcode){
-    return JSON.parse(request('GET', "http://api.rightmove.co.uk/api/sale/find?index=0&sortType=2&numberOfPropertiesRequested=9999&locationIdentifier=OUTCODE%5E"+outcodes[outcode]+"&apiApplication=IPAD").getBody()).properties;
+    return JSON.parse(request('GET', "http://api.rightmove.co.uk/api/sale/find?index=0&sortType=2&numberOfPropertiesRequested=9999&locationIdentifier=OUTCODE%5E"+(outcodes[outcode]||outcode)+"&apiApplication=IPAD").getBody()).properties;
   },
   detail:function(id){
     return JSON.parse(request('GET', "http://api.rightmove.co.uk:80/api/propertyDetails?propertyId="+id+"&apiApplication=IPAD").getBody());
   },
   areaDetail:function(outcode){
-    return JSON.parse(request('GET', "http://api.rightmove.co.uk/api/sale/find?index=0&sortType=2&numberOfPropertiesRequested=0&locationIdentifier=OUTCODE%5E"+outcodes[outcode]+"&apiApplication=IPAD").getBody());
+    return JSON.parse(request('GET', "http://api.rightmove.co.uk/api/sale/find?index=0&sortType=2&numberOfPropertiesRequested=0&locationIdentifier=OUTCODE%5E"+(outcodes[outcode]||outcode)+"&apiApplication=IPAD").getBody());
   },
   rmLocIdentFromOutcode:function(outcode){
     return outcodes[outcode];
