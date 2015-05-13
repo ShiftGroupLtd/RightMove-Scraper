69db56a04988db15c47f61c0eb4bb19464766fff
Joe Reeve <joe@wigwamm.com>
2015-05-13 11:53:37 +0100

    Added paging

--- RightMoveScraper.js
+++ RightMoveScraper.js
@@ -6,10 +6,9 @@ db.rightmoveCodes.find({}, function(err, docs){
     outcodes[val.outcode]=val.locationIdent;
   });
 });
-
 module.exports = {
-  byOutcode:function(outcode){
-    return JSON.parse(request('GET', "http://api.rightmove.co.uk/api/sale/find?index=0&sortType=2&numberOfPropertiesRequested=9999&locationIdentifier=OUTCODE%5E"+(outcodes[outcode]||outcode)+"&apiApplication=IPAD").getBody()).properties;
+  byOutcode:function(outcode, page){
+    return JSON.parse(request('GET', "http://api.rightmove.co.uk/api/sale/find?index="+page*100+"&sortType=2&numberOfPropertiesRequested=9999&locationIdentifier=OUTCODE%5E"+(outcodes[outcode]||outcode)+"&apiApplication=IPAD").getBody()).properties;
   },
   detail:function(id){
     return JSON.parse(request('GET', "http://api.rightmove.co.uk:80/api/propertyDetails?propertyId="+id+"&apiApplication=IPAD").getBody());
